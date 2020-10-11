25c494b1e0a1c496ce5c3702efeab57dcdfb1c80
Daniel Szuster <37206928+SzustyDaniel@users.noreply.github.com>
2020-10-11 13:37:05 +0300

    Created monitor module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'monitor', loadChildren: () => import('./monitor/monitor.module').then(m => m.MonitorModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
