0b812fec38906ce032f60e219b8c31a44447edbb
Daniel Szuster <37206928+SzustyDaniel@users.noreply.github.com>
2020-10-11 15:33:20 +0300

    started work on monitor type selection.

--- src/app/monitor/monitor-type-selection/monitor-type-selection.component.ts
+++ src/app/monitor/monitor-type-selection/monitor-type-selection.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MonitorService } from "../monitor.service";
 
 @Component({
-  selector: 'app-monitor-type-selection',
-  templateUrl: './monitor-type-selection.component.html',
-  styleUrls: ['./monitor-type-selection.component.scss']
+  selector: "app-monitor-type-selection",
+  templateUrl: "./monitor-type-selection.component.html",
+  styleUrls: ["./monitor-type-selection.component.scss"],
 })
 export class MonitorTypeSelectionComponent implements OnInit {
+  constructor(private monitorsService: MonitorService) {}
 
-  constructor() { }
+  monitors$ = this.monitorsService.getLegendFromApi();
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
