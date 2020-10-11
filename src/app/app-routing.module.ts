dc0ac116364d34c79522b51e16790f9b05edb1af
Daniel Szuster <37206928+SzustyDaniel@users.noreply.github.com>
2020-10-11 13:43:32 +0300

    added route redirect for the application. created base display for the monitor feature.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [{ path: 'monitor', loadChildren: () => import('./monitor/monitor.module').then(m => m.MonitorModule) }];
+const routes: Routes = [
+  {
+    path: "monitor",
+    loadChildren: () =>
+      import("./monitor/monitor.module").then((m) => m.MonitorModule),
+  },
+  { path: "", redirectTo: "monitor", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
